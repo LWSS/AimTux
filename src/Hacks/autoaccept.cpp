c43c2bced09f553fba2bbd45dbe99d703c0f9055
Electric26 <clivecdd@gmail.com>
2018-12-20 20:00:44 -0700

    Update autoaccept.cpp

--- src/Hacks/autoaccept.cpp
+++ src/Hacks/autoaccept.cpp
@@ -7,7 +7,7 @@ void AutoAccept::EmitSound(const char* pSoundEntry)
 	if (!Settings::AutoAccept::enabled)
 		return;
 
-	if (strcmp(pSoundEntry, XORSTR("UIPanorama.popup_accept_match_beep")) == 0 && SetLocalPlayerReady)
+	if (SetLocalPlayerReady && strcmp(pSoundEntry, XORSTR("UIPanorama.popup_accept_match_beep")) == 0)
 	{
 		SetLocalPlayerReady("");
 	}
